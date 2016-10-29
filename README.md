fae7b933a6b281b52837ac30f4136af016a53aa0
Sergey Usachev <sergey@usachev.net>
2016-10-29 20:39:02 +0500

    Implementation logging

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # autoschedule
 Transport departure schedule
+
+use environment variable TDS_ROOT="root of project" 
+(for example TDS_ROOT="d:\java\Projects\autoschedule\")
+
--- src/main/java/net/usachev/autoschedule/web/UserServlet.java
+++ src/main/java/net/usachev/autoschedule/web/UserServlet.java
@@ -1,14 +1,21 @@
 package net.usachev.autoschedule.web;
 
+import org.slf4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
+
 public class UserServlet extends HttpServlet {
+    private static final Logger LOG = getLogger(UserServlet.class);
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.debug("Redirect to userList");
         request.getRequestDispatcher("/userList.jsp").forward(request, response);
     }
 }
